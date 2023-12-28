a0025ec47765f9c9c32aae861ea89e89859b1eb1
Peter Zainzinger <peterzainzinger@gmail.com>
2023-12-28 14:12:55 +0100

    fix query typing

--- apps/electric-drizzle/_components/Example.tsx
+++ apps/electric-drizzle/_components/Example.tsx
@@ -8,7 +8,12 @@ import { ElectricDatabase, electrify } from 'electric-sql/wa-sqlite';
 import { authToken } from './auth';
 import { Electric } from '../client/client';
 import { drizzleBuilder } from '../client/drizzle';
-import { schema, tableComments } from '../client/tables';
+import { schema, tableComments, tableReactions } from '../client/tables';
+import { SQLiteSelect } from 'drizzle-orm/sqlite-core';
+import { Row, Statement } from 'electric-sql/dist/util';
+import { LiveResultContext } from 'electric-sql/dist/client/model/model';
+import { SelectResult } from 'drizzle-orm/query-builders/select.types';
+import { eq, sql } from 'drizzle-orm';
 
 const { ElectricProvider, useElectric } = makeElectricContext<Electric>();
 
@@ -56,19 +61,70 @@ export const Example = () => {
   );
 };
 
-const ExampleComponent = () => {
-  const { db, satellite } = useElectric()!;
-  const drizzleDb = useMemo(() => drizzleBuilder(db, satellite), [db]);
-  const selectQuery = useMemo(
-    () => drizzleDb.select().from(tableComments).toSQL(),
-    [db]
-  );
+type QueryResult<T> = {
+  results: T;
+  sql: string;
+  params: any[];
+};
+
+type RunResult<T extends SQLiteSelect<any, any, any, any, any>> =
+  T extends SQLiteSelect<
+    infer TTableName,
+    infer TResultType,
+    infer TRunResult,
+    infer TSelection,
+    infer TSelectMode,
+    infer TNullabilityMap
+  >
+    ? SelectResult<TSelection, TSelectMode, TNullabilityMap>
+    : never;
+
+function useDrizzleLive<
+  const T extends SQLiteSelect<string, any, any, any, any>
+>(
+  db: {
+    liveRaw(sql: Statement): LiveResultContext<any>;
+  },
+  rawQuery: T
+): RunResult<T>[] | undefined {
+  const selectQuery = useMemo(() => rawQuery.toSQL(), [rawQuery]);
   const { results } = useLiveQuery(
     db.liveRaw({
       sql: selectQuery.sql,
       args: selectQuery.params as any,
     })
   );
+
+  return results as any;
+}
+
+const ExampleComponent = () => {
+  const { db, satellite } = useElectric()!;
+  const drizzleDb = useMemo(() => drizzleBuilder(db, satellite), [db]);
+  const rawQuery = useMemo(() => {
+    const countReactions = drizzleDb
+      .select({
+        commentId: tableReactions.comment_id,
+        count: sql<number>`count(
+                ${tableReactions.id}
+                )`
+          .mapWith(Number)
+          .as('count'),
+      })
+      .from(tableReactions)
+      .groupBy(tableReactions.comment_id)
+      .as('reactionCounts');
+
+    return drizzleDb
+      .select({
+        id: tableComments.id,
+        text: tableComments.text,
+        count: countReactions.count,
+      })
+      .from(tableComments)
+      .leftJoin(countReactions, eq(countReactions.commentId, tableComments.id));
+  }, [db]);
+  const results = useDrizzleLive(db, rawQuery);
 
   useEffect(() => {
     const syncItems = async () => {
@@ -111,9 +167,23 @@ const ExampleComponent = () => {
           Clear
         </button>
       </div>
-      {items.map((item: any, index: number) => (
+      {items.map((item, index: number) => (
         <p key={index} className="item">
-          <code>{item.text}</code>
+          <code>
+            {item.text} - {item.count || 'none'}
+          </code>
+
+          <button
+            onClick={async () => {
+              await drizzleDb.insert(tableReactions).values({
+                id: genUUID(),
+                comment_id: item.id!,
+                text: 'like',
+              });
+            }}
+          >
+            Click me
+          </button>
         </p>
       ))}
     </div>
